006478d4afee81d1f13e3fa7ec69de83c585ccd8
Patrick Clarke <ekralccp@me.com>
2016-02-15 13:38:47 -0800

    Switched petro post to line plot

--- _includes/2016/02/petroChart.js
+++ _includes/2016/02/petroChart.js
@@ -1,202 +1,131 @@
 var petroChart = function() {
-	//Set up stack method
-	var stack = d3.layout.stack()
-					.values(function(d) {
-						return d.emissions;
-					});
+  var years,
+      yearFormat = d3.time.format("%Y");
+			
+	var numFormat = d3.format(",.0");
 
-	//Width, height, padding
-	var padding = {top: 20, right: 10, bottom: 50, left: 50},
-		w = 740 - padding.left - padding.right,
-		h = 400 - padding.top - padding.bottom;
+  var margin = {top: 10, right: 20, bottom: 30, left: 50},
+      width = 740 - margin.left - margin.right,
+      height = 500 - margin.top - margin.bottom;
 
-	//Set up scales
-	var xScale = d3.scale.ordinal()
-						.rangePoints([ 0, w ]);
+  var x = d3.scale.ordinal()
+      .rangeBands([0, width]);
 
-	var yScale = d3.scale.linear()
-						.range([ 0, h]);
+  var y = d3.scale.linear()
+      .range([height, 0]);
 
-	//Configure axis generators
-	var xAxis = d3.svg.axis()
-					.scale(xScale)
-					.orient("bottom");
+  var voronoi = d3.geom.voronoi()
+      .x(function(d) { return x(d.date); })
+      .y(function(d) { return y(d.value); })
+      .clipExtent([[-margin.left, -margin.top], [width + margin.right, height + margin.bottom]]);
 
-	var yAxis = d3.svg.axis()
-					.scale(yScale)
-					.orient("left")
-					.ticks(5);
+  var line = d3.svg.line()
+      .x(function(d) { return x(d.date); })
+      .y(function(d) { return y(d.value); });
 
-	var coordinates = [0, 0];
+  var svg = d3.select("#petroChart").append("svg")
+      .attr("class", "budgetPlotted")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-	var body = d3.select("body")
-		.on("mousemove", function() {
-			coordinates = d3.mouse(this);
-		})
-		.on("mousedown", function() {
-			coordinates = d3.mouse(this);
-		});
+  d3.csv("{{ site.baseurl }}/data/2016/02/canadian_wages.csv", type, function(error, data) {
+    x.domain(years);
+    y.domain([d3.min(data, function(c) { 
+        return d3.min(c.values, function(d) { return d.value; }); 
+      }),
+			d3.max(data, function(c) {
+	      return d3.max(c.values, function(d) { return d.value; }); 
+	    })]).nice();
 
-	var preArea = d3.svg.area()
-		.x(function(d) {
-			return xScale(d.x);
-		})
-		.y0(function(d) {
-			return yScale(0);  //Updated
-		})
-		.y1(function(d) {
-			return yScale(0);  //Updated
-		});
+    svg.append("g")
+        .attr("class", "axis axis--x")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.svg.axis()
+          .scale(x)
+          .orient("bottom")
+          .tickValues(years.filter(function(y, i) { 
+          	if (i % 10 == 0) return y; 
+          }))
+        );
 
-	var area = d3.svg.area()
-		.x(function(d) {
-			return xScale(d.x);
-		})
-		.y0(function(d) {
-			return yScale(d.y0);  //Updated
-		})
-		.y1(function(d) {
-			return yScale(d.y0 + d.y);  //Updated
-		});
+    svg.append("g")
+        .attr("class", "axis axis--y")
+        .call(d3.svg.axis()
+          .scale(y)
+          .orient("left"))
+        	    .append("text")
+	      .attr("transform", "rotate(-90)")
+	      .attr("y", 6)
+	      .attr("dy", ".71em")
+	      .style("text-anchor", "end")
+	      .text("Million dollars");;
 
-	var color = d3.scale.category10();
-	var serviceColor = d3.scale.ordinal()
-		.range(colorbrewer.Greys[9]);
-	var goodsColor = d3.scale.ordinal()
-		.range(colorbrewer.Greys[5]);
+    svg.append("g")
+      .selectAll("path")
+        .data(data)
+      .enter().append("path")
+        .attr("d", function(d) { d.line = this; return line(d.values); })
+		.attr("class", "data");
 
-	var svg = d3.select("#petroChart")
-				.append("svg")
-				.attr("width", w + padding.left + padding.right)
-				.attr("height", h + padding.top + padding.bottom)
-			.append("g")
-				.attr("transform", "translate(" + padding.left + "," + padding.top + ")")
-				.attr("class", "stacked");
+    var focus = svg.append("g")
+        .attr("transform", "translate(-100,-100)")
+        .attr("class", "focus");
 
-	d3.csv("{{ site.baseurl }}/data/2016/02/canadian_wages.csv", function(data) {
+    focus.append("circle")
+        .attr("r", 3.5);
 
-		var quarters = d3.keys(data[0]).filter(function(d) { return d !== "Industry" && d !== "Type"; });
+    focus.append("text")
+        .attr("y", -10);
 
-		var dataset = [];
-		var servIndustries = [];
-		var goodIndustries = [];
+    var voronoiGroup = svg.append("g")
+        .attr("class", "voronoi");
 
-		//Loop once for each row in data
-		for (var i = 0; i < data.length; i++) {
+    voronoiGroup.selectAll("path")
+      .data(voronoi(d3.nest()
+        .key(function(d) { return x(d.date) + "," + y(d.value); })
+        .rollup(function(v) { return v[0]; })
+        .entries(d3.merge(data.map(function(d) { return d.values; })))
+        .map(function(d) { return d.values; })))
+      .enter().append("path")
+        .attr("d", function(d) { return "M" + d.join("L") + "Z"; })
+        .datum(function(d) { return d.point; })
+        .on("mouseover", mouseover)
+        .on("mouseout", mouseout);
 
-			//Create new object with this country's name and empty array
-			dataset[i] = {
-				country: data[i].Industry,
-				type: data[i].Type,
-				emissions: []
-			};
-
-			if (data[i].Type === "Goods-producing") {
-				goodIndustries.push(data[i].Industry);
-			} else {
-				servIndustries.push(data[i].Industry);
-			}
-
-			//Loop through all the years
-			for (var j = 0; j < quarters.length; j++) {
-
-				//Default value, used in case no value is present
-				var amount = null;
-
-				// If value is not empty
-				if (data[i][quarters[j]]) {
-					amount = +data[i][quarters[j]];
-				}
-
-				//Add a new object to the emissions data array
-				//for this country
-				dataset[i].emissions.push({
-					x: quarters[j],
-					y: amount
-				});
-
-			}
-		}
-
-		stack(dataset);
-
-		xScale.domain(quarters);
-
-		var tickFilter = xScale.domain().filter(function(d) { return (+d.substring(0, 4) % 5) == 0 && d.substring(d.length - 2) === "Q1"; } );
-		xAxis.tickValues(tickFilter);
-
-		var totals = [];
-
-		for (i = 0; i < quarters.length; i++) {
-			totals[i] = 0;
-			for (j = 0; j < dataset.length; j++) {
-				totals[i] += dataset[j].emissions[i].y;
-			}
-		}
-
-		yScale.domain([ d3.max(totals), 0 ]);
-
-		serviceColor.domain(servIndustries);
-		goodsColor.domain(goodIndustries);
-
-		var paths = svg.selectAll("path")
-			.data(dataset)
-			.enter()
-			.append("path")
-			.attr("class", "area")
-			.attr("d", function(d) {
-				//Calculate path based on only d.emissions array,
-				//not all of d (which would include the country name)
-				return preArea(d.emissions);
-			})
-			.attr("stroke", "none")
-			.attr("fill", function(d, i) {
-				if (d.type === "Goods-producing") {
-					return goodsColor(d.country);
-				} else {
-					return serviceColor(d.country);
-				}
-			})
-			.on("mouseover", function(d) {
-				d3.select(".tooltip")
-					.style("left", coordinates[0] + "px")
-					.style("top", coordinates[1] + "px");
+    function mouseover(d) {
+      d3.select("#petroTip").select("#wageType")
+				.text(d.budget.name + " " + d.date);
 				
-				d3.select(".tooltip")
-					.select("#wageType").text(d.country);
+      d3.select("#petroTip").select("#wageAmount")
+				.text("$" + numFormat(Math.abs(d.value)) + " million dollars");
 
-				d3.select(".tooltip").classed("hidden", false);
-			})
-			.on("mouseout", function(d) {
-				d3.select(".tooltip").classed("hidden", true);	
-			});
+      d3.select(d.budget.line).classed("budget--hover", true);
+      d.budget.line.parentNode.appendChild(d.budget.line);
+      focus.attr("transform", "translate(" + x(d.date) + "," + y(d.value) + ")");
+    }
 
-		paths.transition()
-			.duration(2000)
-			.attr("d", function(d) {
-				return area(d.emissions);
-			})
+    function mouseout(d) {
+      d3.select(d.budget.line).classed("budget--hover", false);
+      focus.attr("transform", "translate(-100,-100)");
+    }
+  });
 
-		paths.append("title")
-			.text(function(d) {
-				return d.country;
-			});
-
-		//Create axes
-		svg.append("g")
-			.attr("class", "x axis")
-			.attr("transform", "translate(0," + h + ")")
-			.call(xAxis);
-
-		svg.append("g")
-				.attr("class", "y axis")
-				.call(yAxis)
-			.append("text")
-				.attr("transform", "rotate(-90)")
-				.attr("y", 6)
-				.attr("dy", ".71em")
-				.style("text-anchor", "end")
-				.text("Million dollars");
-
-	});
+  function type(d, i) {
+    if (!i) years = Object.keys(d).filter(function(key) { return key !== "Industry" && key!== "Type"; });
+    var budget = {
+      name: d.Industry,
+      values: null
+    };
+    budget.values = years.map(function(m) {
+    	
+      return {
+        budget: budget,
+        date: m,
+        value: (+d[m])
+      };
+    });
+    return budget;
+  }
 }();
