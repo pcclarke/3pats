8fd996ea6bf3304f88d63834421f812d23455c11
Patrick Clarke <ekralccp@me.com>
2016-02-17 23:05:02 -0800

    New post for gas royalties

---
+++ _includes/2016/02/bcgas1.js
@@ -0,0 +1,136 @@
+var bcgas = function() {
+  var years,
+      yearFormat = d3.time.format("%Y");
+			
+	var numFormat = d3.format(",.0");
+
+  var margin = {top: 10, right: 20, bottom: 30, left: 50},
+      width = 740 - margin.left - margin.right,
+      height = 400 - margin.top - margin.bottom;
+
+  var x = d3.scale.ordinal()
+      .rangePoints([0, width]);
+
+  var y = d3.scale.linear()
+      .range([height, 0]);
+      
+  var voronoi = d3.geom.voronoi()
+      .x(function(d) { return x(d.date); })
+      .y(function(d) { return y(d.value); })
+      .clipExtent([[-margin.left, -margin.top], [width + margin.right, height + margin.bottom]]);
+      
+  var lineFlat = d3.svg.line()
+      .x(function(d) { return x(d.date); })
+      .y(function(d) { return y(0); });
+
+  var line = d3.svg.line()
+      .x(function(d) { return x(d.date); })
+      .y(function(d) { return y(d.value); });
+
+  var svg = d3.select("#bcgasChart").append("svg")
+      .attr("class", "budgetPlotted")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+	d3.csv("{{ site.baseurl }}/data/2016/02/bc_gas_royalties.csv", type, function(error, data) {
+		x.domain(years);
+		y.domain([d3.min(data, function(c) { 
+			return d3.min(c.values, function(d) { return d.value; }); 
+		}), d3.max(data, function(c) {
+			return d3.max(c.values, function(d) { return d.value; }); 
+	    })]).nice();
+	    
+	    console.log(x("2009/10"));
+
+		svg.append("g")
+			.attr("class", "axis axis--x")
+        	.attr("transform", "translate(0," + height + ")")
+        	.call(d3.svg.axis()
+          	.scale(x)
+          	.orient("bottom")
+        );
+
+    svg.append("g")
+        .attr("class", "axis axis--y")
+        .call(d3.svg.axis()
+          .scale(y)
+          .orient("left"))
+        	    .append("text")
+	      .attr("transform", "rotate(-90)")
+	      .attr("y", 6)
+	      .attr("dy", ".71em")
+	      .style("text-anchor", "end")
+	      .text("Million dollars");
+
+    var lines = svg.append("g")
+      .selectAll("path")
+        .data(data)
+      .enter().append("path")
+        .attr("d", function(d) { d.line = this; return lineFlat(d.values); })
+		.attr("class", "data");
+		
+	lines.transition()
+		.duration(1000)
+		.attr("d", function(d) { d.line = this; return line(d.values); });
+
+    var focus = svg.append("g")
+        .attr("transform", "translate(-100,-100)")
+        .attr("class", "focus");
+
+    focus.append("circle")
+        .attr("r", 3.5);
+
+    focus.append("text")
+        .attr("y", -10);
+
+    var voronoiGroup = svg.append("g")
+        .attr("class", "voronoi");
+
+   	voronoiGroup.selectAll("path")
+      .data(voronoi(d3.nest()
+        .key(function(d) { return x(d.date) + "," + y(d.value); })
+        .rollup(function(v) { return v[0]; })
+        .entries(d3.merge(data.map(function(d) { return d.values; })))
+        .map(function(d) { return d.values; })))
+      .enter().append("path")
+        .attr("d", function(d) { return "M" + d.join("L") + "Z"; })
+        .datum(function(d) { return d.point; })
+        .on("mouseover", mouseover)
+        .on("mouseout", mouseout);
+
+    function mouseover(d) {
+      d3.select("#bcgasTip").select("#bcgasBudget")
+				.text(d.budget.name + " " + d.date);
+				
+      d3.select("#bcgasTip").select("#bcgasVal")
+				.text("$" + numFormat(Math.abs(d.value)) + " million dollars");
+
+      d3.select(d.budget.line).classed("budget--hover", true);
+      d.budget.line.parentNode.appendChild(d.budget.line);
+      focus.attr("transform", "translate(" + x(d.date) + "," + y(d.value) + ")");
+    }
+
+    function mouseout(d) {
+      d3.select(d.budget.line).classed("budget--hover", false);
+      focus.attr("transform", "translate(-100,-100)");
+    }
+  });
+
+  function type(d, i) {
+    if (!i) years = Object.keys(d).filter(function(key) { return key !== "Natural gas royalties"; });
+    var budget = {
+      name: d["Natural gas royalties"],
+      values: null
+    };
+    budget.values = years.map(function(m) {
+      return {
+        budget: budget,
+        date: m,
+        value: (+d[m])
+      };
+    });
+    return budget;
+  }
+}();
