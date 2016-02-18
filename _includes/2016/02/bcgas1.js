e768c9a5550a3a3197d93bec1a5fddb912efef72
Patrick Clarke <ekralccp@me.com>
2016-02-18 14:17:06 -0800

    Switched natural gas chart to difference

--- _includes/2016/02/bcgas1.js
+++ _includes/2016/02/bcgas1.js
@@ -1,136 +1,142 @@
 var bcgas = function() {
-  var years,
-      yearFormat = d3.time.format("%Y");
-			
-	var numFormat = d3.format(",.0");
+	var margin = {top: 20, right: 20, bottom: 30, left: 50},
+		width = 740 - margin.left - margin.right,
+		height = 400 - margin.top - margin.bottom;
 
-  var margin = {top: 10, right: 20, bottom: 30, left: 50},
-      width = 740 - margin.left - margin.right,
-      height = 400 - margin.top - margin.bottom;
+	var parseDate = d3.time.format("%Y").parse;
 
-  var x = d3.scale.ordinal()
-      .rangePoints([0, width]);
+	var x = d3.time.scale()
+    	.range([0, width]);
 
-  var y = d3.scale.linear()
-      .range([height, 0]);
-      
-  var voronoi = d3.geom.voronoi()
-      .x(function(d) { return x(d.date); })
-      .y(function(d) { return y(d.value); })
-      .clipExtent([[-margin.left, -margin.top], [width + margin.right, height + margin.bottom]]);
-      
-  var lineFlat = d3.svg.line()
-      .x(function(d) { return x(d.date); })
-      .y(function(d) { return y(0); });
-
-  var line = d3.svg.line()
-      .x(function(d) { return x(d.date); })
-      .y(function(d) { return y(d.value); });
-
-  var svg = d3.select("#bcgasChart").append("svg")
-      .attr("class", "budgetPlotted")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-	d3.csv("{{ site.baseurl }}/data/2016/02/bc_gas_royalties.csv", type, function(error, data) {
-		x.domain(years);
-		y.domain([d3.min(data, function(c) { 
-			return d3.min(c.values, function(d) { return d.value; }); 
-		}), d3.max(data, function(c) {
-			return d3.max(c.values, function(d) { return d.value; }); 
-	    })]).nice();
-	    
-	    console.log(x("2009/10"));
-
-		svg.append("g")
-			.attr("class", "axis axis--x")
-        	.attr("transform", "translate(0," + height + ")")
-        	.call(d3.svg.axis()
-          	.scale(x)
-          	.orient("bottom")
-        );
-
-    svg.append("g")
-        .attr("class", "axis axis--y")
-        .call(d3.svg.axis()
-          .scale(y)
-          .orient("left"))
-        	    .append("text")
-	      .attr("transform", "rotate(-90)")
-	      .attr("y", 6)
-	      .attr("dy", ".71em")
-	      .style("text-anchor", "end")
-	      .text("Million dollars");
-
-    var lines = svg.append("g")
-      .selectAll("path")
-        .data(data)
-      .enter().append("path")
-        .attr("d", function(d) { d.line = this; return lineFlat(d.values); })
-		.attr("class", "data");
+	var y = d3.scale.linear()
+		.range([height, 0]);
 		
-	lines.transition()
-		.duration(1000)
-		.attr("d", function(d) { d.line = this; return line(d.values); });
+	var M;
 
-    var focus = svg.append("g")
-        .attr("transform", "translate(-100,-100)")
-        .attr("class", "focus");
+	var xAxis = d3.svg.axis()
+		.scale(x)
+		.orient("bottom");
 
-    focus.append("circle")
-        .attr("r", 3.5);
+	var yAxis = d3.svg.axis()
+		.scale(y)
+		.orient("left");
 
-    focus.append("text")
-        .attr("y", -10);
+	var line = d3.svg.area()
+		.interpolate("basis")
+		.x(function(d) { return x(d.Year); })
+		.y(function(d) { return y(d["Budget 2016"]); });
 
-    var voronoiGroup = svg.append("g")
-        .attr("class", "voronoi");
+	var area = d3.svg.area()
+		.interpolate("basis")
+		.x(function(d) { return x(d.Year); })
+		.y1(function(d) { return y(d["Budget 2016"]); });
 
-   	voronoiGroup.selectAll("path")
-      .data(voronoi(d3.nest()
-        .key(function(d) { return x(d.date) + "," + y(d.value); })
-        .rollup(function(v) { return v[0]; })
-        .entries(d3.merge(data.map(function(d) { return d.values; })))
-        .map(function(d) { return d.values; })))
-      .enter().append("path")
-        .attr("d", function(d) { return "M" + d.join("L") + "Z"; })
-        .datum(function(d) { return d.point; })
-        .on("mouseover", mouseover)
-        .on("mouseout", mouseout);
+	var svg = d3.select("#bcgasChart").append("svg")
+		.attr("width", width + margin.left + margin.right)
+		.attr("height", height + margin.top + margin.bottom)
+	  .append("g")
+		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    function mouseover(d) {
-      d3.select("#bcgasTip").select("#bcgasBudget")
-				.text(d.budget.name + " " + d.date);
+	d3.csv("{{ site.baseurl }}/data/2016/02/bc_gas_royalties_t.csv", type, function(error, data) {
+	  if (error) throw error;
+
+		x.domain(d3.extent(data, function(d) { return d.Year; }));
+
+	  y.domain([
+		d3.min(data, function(d) { return Math.min(d["Budget 2016"], d["Budget 2015"]); }),
+		d3.max(data, function(d) { return Math.max(d["Budget 2016"], d["Budget 2015"]); })
+	  ]);
+
+	  svg.datum(data);
+
+	  svg.append("clipPath")
+		  .attr("id", "clip-below")
+		.append("path")
+		  .attr("d", area.y0(height));
+
+	  svg.append("clipPath")
+		  .attr("id", "clip-above")
+		.append("path")
+		  .attr("d", area.y0(0));
+
+	  svg.append("path")
+		  .attr("class", "area above")
+		  .attr("clip-path", "url(#clip-above)")
+		  .attr("d", area.y0(function(d) { return y(d["Budget 2015"]); }))
+		  .on("mouseover", function(d) {
+
+		  });
+
+	  svg.append("path")
+		  .attr("class", "area below")
+		  .attr("clip-path", "url(#clip-below)")
+		  .attr("d", area);
+
+	  svg.append("path")
+		  .attr("class", "line")
+		  .attr("d", line);
+
+	  svg.append("g")
+		  .attr("class", "x axis")
+		  .attr("transform", "translate(0," + height + ")")
+		  .call(xAxis);
+
+	  svg.append("g")
+		  .attr("class", "y axis")
+		  .call(yAxis)
+		.append("text")
+		  .attr("transform", "rotate(-90)")
+		  .attr("y", 6)
+		  .attr("dy", ".71em")
+		  .style("text-anchor", "end")
+		  .text("Million dollars");
+		  
+		var vertical = svg.append("line")
+			.attr("x1", 0)
+			.attr("y1", y(y.domain()[0]))
+			.attr("x2", 0)
+			.attr("y2", y(y.domain()[1]))
+			.attr("class", "vertical");
+			
+		d3.select("#bcgasChart")
+		  .on("mousemove", function(){
+			 M = d3.mouse(svg[0][0]);
+			 vertical.attr("x1", M[0])
+			 vertical.attr("x2", M[0])
+			 updateTip();
+			 })
+		  .on("mouseover", function(){  
+			 M = d3.mouse(svg[0][0]);
+			 vertical.attr("x1", M[0])
+			 vertical.attr("x2", M[0])
+			 updateTip();
+			});
+			
+		function updateTip() {
+			var selYear = x.invert(M[0]).getFullYear();
+			if (selYear >= 2009 && selYear <= 2017) {
+				d3.select("#bcgasTip").classed("hidden", false);
 				
-      d3.select("#bcgasTip").select("#bcgasVal")
-				.text("$" + numFormat(Math.abs(d.value)) + " million dollars");
-
-      d3.select(d.budget.line).classed("budget--hover", true);
-      d.budget.line.parentNode.appendChild(d.budget.line);
-      focus.attr("transform", "translate(" + x(d.date) + "," + y(d.value) + ")");
-    }
-
-    function mouseout(d) {
-      d3.select(d.budget.line).classed("budget--hover", false);
-      focus.attr("transform", "translate(-100,-100)");
-    }
-  });
-
-  function type(d, i) {
-    if (!i) years = Object.keys(d).filter(function(key) { return key !== "Natural gas royalties"; });
-    var budget = {
-      name: d["Natural gas royalties"],
-      values: null
-    };
-    budget.values = years.map(function(m) {
-      return {
-        budget: budget,
-        date: m,
-        value: (+d[m])
-      };
-    });
-    return budget;
-  }
+				d3.select("#bcgasTip").select("#bcgasBudget")
+					.text(x.invert(M[0]).getFullYear() + "/" + (x.invert(M[0]).getFullYear() - 1999));
+				
+				d3.select("#bcgasTip").select("#bcgasVal2015")
+					.text(data[selYear - 2009]["Budget 2015"]);
+				
+				d3.select("#bcgasTip").select("#bcgasVal2016")
+					.text(data[selYear - 2009]["Budget 2016"]);
+					
+				d3.select("#bcgasTip").select("#bcgasDiff")
+					.text(data[selYear - 2009]["Budget 2016"] - data[selYear - 2009]["Budget 2015"]);
+			}
+		}
+	});
+	
+	function type(d) {
+		d.Year = parseDate(d.Year.substr(0, 4));
+		d["Budget 2015"] = +d["Budget 2015"];
+		d["Budget 2016"] = +d["Budget 2016"];
+		
+		return d;
+	}
 }();
